a710fb47a6bc661d4c2705a4a09dd63802633d43
yantao.chen <Aa86358201>
2018-07-18 17:18:18 +0800

    angularjs项目初始化

---
+++ js/app.js
@@ -0,0 +1,65 @@
+// var phonecatApp = angular.module('phonecatApp', [
+//     'ngRoute',
+//     'phonecatControllers'
+//   ]);
+
+// phonecatApp.config(['$routeProvider',
+// function($routeProvider) {
+//     $routeProvider.
+//         when('/phones', {
+//             templateUrl: 'phones/phone-list.html',
+//             controller: 'PhoneListCtrl'
+//         }).
+//         otherwise({
+//             redirectTo: '/phones'
+//         });
+// }]);
+
+var app = angular.module('phonecatApp',["ui.router","oc.lazyLoad"]);
+
+// app.config(
+//     ['$controllerProvider', '$compileProvider', '$filterProvider', '$provide',
+//         function ($controllerProvider, $compileProvider, $filterProvider, $provide) {
+//             app.controller = $controllerProvider.register;
+//             app.directive = $compileProvider.directive;
+//             app.filter = $filterProvider.register;
+//             app.factory = $provide.factory;
+//             app.service = $provide.service; 
+//             app.constant = $provide.constant;
+//             app.value = $provide.value;
+//         }
+//     ]
+// );
+
+app.config(function($controllerProvider, $compileProvider, $filterProvider, $provide) {
+    app.register = {
+      controller: $controllerProvider.register,
+      directive: $compileProvider.directive,
+      filter: $filterProvider.register,
+      factory: $provide.factory,
+      service: $provide.service
+    };
+  });
+
+app.config(['$ocLazyLoadProvider', function ($ocLazyLoadProvider) {
+    $ocLazyLoadProvider.config({
+        debug: true,
+        events: true
+    });
+}]);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state("phones", {
+        url: "/phones",
+        templateUrl: "model/phone-list.html",
+        resolve: {
+            deps: ['$ocLazyLoad',
+                function ($ocLazyLoad) {
+                    return $ocLazyLoad.load(['js/controllers/mobile_controller.js'], {cache: false});
+                }
+            ]
+        }
+    });
+
+});
---
+++ js/controllers/mobile_controller.js
@@ -0,0 +1,26 @@
+app.controller('PhoneListCtrl', function ($scope,$http) {
+  // $scope.phones = [
+  //   {'name': 'Nexus S',
+  //    'snippet': 'Fast just got faster with Nexus S.',
+  //    'age':'1'},
+  //   {'name': 'Motorola XOOM? with Wi-Fi',
+  //    'snippet': 'The Next, Next Generation tablet.',
+  //    'age':'3'},
+  //   {'name': 'MOTOROLA XOOM?',
+  //    'snippet': 'The Next, Next Generation tablet.',
+  //    'age':'2'}
+  // ];
+
+  $http.get('../static/phones.json').success(function(data) {
+    $scope.phones = data;
+  });
+
+  $scope.names = [
+      1,2,3,4,5,6,7,8,9,0
+  ]
+  $scope.orderProp = 'age';
+
+  $scope.aler = function(){
+    alert(123);
+  }
+});
